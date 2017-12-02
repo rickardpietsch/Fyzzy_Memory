0925506cb4c4ac2756d98ea9a2b949d96e5e5982
Rickard Pietsch <rickard.pietsch@gmail.com>
2017-12-02 17:55:21 +0100

    Shuffle function added

--- logic.js
+++ logic.js
@@ -1,19 +1,34 @@
 const cards = ['A','B','C','D','E','F','G','H'];
 const gameboard = document.querySelector('.gameboard');
-console.log(cards);
+const solutionArray = cards.concat(cards); //Concatenate array to multiply it
+console.log(solutionArray);
+shuffleArray(solutionArray);
+console.log(solutionArray);
 
+// console.log(cards);
 startGame();
 //Start game function
 function startGame() {
   //Clear gameboard
   gameboard.innerHTML = "";
   //Create gameboard
-  for (var i = 0; i <= ((cards.length*2)-1); i++) {
+  for (let i = 0; i <= ((solutionArray.length)-1); i++) {
     letters(i);
   }
 }
 
 function letters(i) {
-  console.log(i);
+  // console.log(i);
   gameboard.innerHTML += '<div class="card">'+i+'</div>';
 }
+
+//Shuffle array content
+function shuffleArray(d) {
+  for (let c = d.length - 1; c > 0; c--) {
+    const b = Math.floor(Math.random() * (c + 1));
+    const a = d[c];
+    d[c] = d[b];
+    d[b] = a;
+  }
+  return d
+};
